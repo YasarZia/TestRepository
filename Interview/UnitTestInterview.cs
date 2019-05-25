0f8d969ae47591f5cd935303bcd7d11dc7045ca7
Yasar Zia <yasar.zia@forensicanalytics.co.uk>
2019-05-25 10:32:11 +0100

    Test_All Fail

--- Interview/UnitTestInterview.cs
+++ Interview/UnitTestInterview.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Interview
@@ -9,13 +8,12 @@ namespace Interview
     public class UnitTestInterview
     {
         private InMemoryRepository<Storeable> repository;
-        private object result;
 
         [TestMethod]
         public void Test_All()
         {
             repository = new InMemoryRepository<Storeable>();
-            result = repository.All();
+            var result = repository.All();
 
             Assert.IsInstanceOfType(result, typeof(IEnumerable<Storeable>));
         }
