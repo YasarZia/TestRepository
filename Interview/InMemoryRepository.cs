096b258dfab7683ee8f6d88736fa38f0e8427c8d
Yasar Zia <yasar.zia@forensicanalytics.co.uk>
2019-05-25 10:29:27 +0100

    Test_All Fail

---
+++ Interview/InMemoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview
+{
+    public class InMemoryRepository<T> : IRepository<T> where T : IStoreable
+    {
+        private List<T> inMemoryList;
+
+        public InMemoryRepository()
+        {
+            inMemoryList = new List<T>();
+        }
+
+        public IEnumerable<T> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IComparable id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindById(IComparable id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Interview/UnitTestInterview.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Interview
+{
+    [TestClass]
+    public class UnitTestInterview
+    {
+        private InMemoryRepository<Storeable> repository;
+        private object result;
+
+        [TestMethod]
+        public void Test_All()
+        {
+            repository = new InMemoryRepository<Storeable>();
+            result = repository.All();
+
+            Assert.IsInstanceOfType(result, typeof(IEnumerable<Storeable>));
+        }
+    }
+}
